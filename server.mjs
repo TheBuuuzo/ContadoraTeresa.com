ea22c142f0f832edbe4c6d7f2ad835255988dedf
Bruno  Ferreira <bruno@contabilidadecastroalves.com.br>
2026-08-07 15:24:11 -0300

    1.1

--- server.mjs
+++ server.mjs
@@ -63,6 +63,8 @@ const MIME = {
   ".webp": "image/webp",
   ".woff": "font/woff",
   ".woff2": "font/woff2",
+  ".txt": "text/plain; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
 };
 
 function sendJson(res, status, body) {
